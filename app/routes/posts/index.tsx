8ba40cd39e2fd49b47b333cfc8d84d9455c56659
aleks <alexandra.kojic@gmail.com>
2023-03-24 02:50:28 +0100

    init commit

---
+++ app/routes/posts/index.tsx
@@ -0,0 +1,32 @@
+import { Link, useLoaderData } from "@remix-run/react";
+import { json } from "@remix-run/node";
+import { getPosts } from "~/models/post.server";
+
+export const loader = async () => {
+  return json({ posts: await getPosts() });
+};
+
+export default function PostsRoute() {
+  const { posts } = useLoaderData<typeof loader>();
+
+  return (
+    <main>
+      <Link to="admin" className="text-red-600 underline">
+        Admin
+      </Link>
+      <h1>Posts</h1>
+      <ul>
+        {posts.map((post: any) => (
+          <li key={post.slug}>
+            <Link
+              to={`/posts/${post.slug}`}
+              className="text-blue-600 underline"
+            >
+              {post.title}
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </main>
+  );
+}
